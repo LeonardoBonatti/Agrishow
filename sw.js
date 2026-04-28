3679e1e4941d45aa36a8f7e96954cfa7ef87863d
Leonardo Bonatti <leonardobonatti@example.com>
2026-04-28 11:18:49 -0300

    update: cat 924 fuel consumption to 9L/h v42

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'sotreq-agrishow-v41';
+const CACHE_NAME = 'sotreq-agrishow-v42';
 const ASSETS_TO_CACHE = [
   './',
-  './index.html?v=41',
-  './css/style.css?v=41',
-  './js/app.js?v=41',
-  './js/data.js?v=41',
+  './index.html?v=42',
+  './css/style.css?v=42',
+  './js/app.js?v=42',
+  './js/data.js?v=42',
   './img/icon-192.png',
   './img/icon-512.png',
   './img/app_icon.png'
